cb6147b6c0b773ed19303714ec2e2113b6c36ea1
menty44 <menty44@gmail.com>
2019-08-27 12:49:31 +0300

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{ jeff }}</h1>
+<!--    <h1>{{fred(mimi)}}</h1>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    let na = 'mmm'
+    return {
+      msg: 'hah',
+      jeff: this.fred(na)
+    }
+  },
+
+  methods: {
+    fred(x) {
+      let j = 'fala ' + x;
+       return j
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
